8e9520ec37c8efe8f0b76e1d2f209bd82a0f0806
Gavin <gavinwzmails@gmail.com>
2020-07-06 15:15:30 +0800

    update

--- ell_to_st/ell_to_st.c
+++ ell_to_st/ell_to_st.c
@@ -19,11 +19,11 @@ int ell_to_st(double **eData, int **offset, int n_row, int n_max, int** ist, int
     int index = 0;
     for(int i = 0; i < n_row; i++){
         for(int j = 0; j < n_max; j++){
-            if(*((int*)offset + i * (n_row - 1) + j) == -1)
+            if(*((int*)offset + i * n_max + j) == -1)
                 break;
             i_st[index] = i;
-            j_st[index] = *((int*)offset + i * (n_row - 1) + j);
-            a_st[index] = *((double*)eData + i * (n_row - 1) + j);
+            j_st[index] = *((int*)offset + i * n_max + j);
+            a_st[index] = *((double*)eData + i * n_max + j);
             index++;
         }
     }
@@ -32,23 +32,6 @@ int ell_to_st(double **eData, int **offset, int n_row, int n_max, int** ist, int
     *ast = a_st;
     return index;
 }
-
-// void st_to_hyb(
-//     int* ist,           //st格式行偏移数组
-//     int* jst,           //st格式列偏移数组
-//     double* ast,        //st格式非零元数组
-//     int n_val,          //st格式非零元个数
-//     int n_row,          //原二维矩阵行数
-//     int n_col,          //原二维矩阵列数
-//     int max,            //用户定义ELL格式行中非零元个数最大值
-//     double*** eData,    //二维数组的指针，保存HYB中ELL数据矩阵
-//     double*** offset,   //二维数组的指针，保存HYB中ELL列偏移矩阵
-//     int** e_ist,        //一维数组的指针，保存HYB中st格式的行偏移数组
-//     int** e_jst,        //一维数组的指针，保存HYB中st格式的列偏移数组
-//     double**e_ast,      //一维数组的指针，保存HYB中st格式的非零元素数组
-//     int* e_n_val        //整型变量指针，保存HYB中st格式的非零元素个数
-// );
-
 
 void st_to_hyb(int* ist, int* jst, double* ast, int n_val, int n_row, 
         int n_col, int max, double*** eData, int*** offset, 
@@ -110,31 +93,31 @@ void st_to_hyb(int* ist, int* jst, double* ast, int n_val, int n_row,
 
 
 int main(){
-    // int offset[4][3] = {0,1,-1,1,2,-1,0,2,3,1,3,-1};
-    // double eData[4][3] = {1,7,0,2,8,0,5,3,9,6,4,0};
-    // int n_row = 4;
-    // int n_max = 3;
-    // int* ist; 
-    // int* jst;
-    // double* ast;
-    // int n_val = ell_to_st((double**)eData, (int**)offset, n_row, n_max, &ist, &jst, &ast);
-    // for(int i = 0; i < n_val; i++){
-    //     printf("%d\t%d\t%lf\n", ist[i], jst[i], ast[i]);
-    // }
-
-    int ist[9] = {0,0,1,1,2,2,2,3,3};
-    int jst[9] = {0,1,1,2,0,2,3,1,3};
-    double ast[9] = {1,7,2,8,5,3,9,6,4};
-    int n_val = 9;
+    int offset[4][3] = {0,1,-1,1,2,-1,0,2,3,1,3,-1};
+    double eData[4][3] = {1,7,0,2,8,0,5,3,9,6,4,0};
     int n_row = 4;
-    int n_col = 4;
-    int max = 2; 
-    double** eData;
-    double** offset; 
-    int* e_ist;
-    int* e_jst; 
-    double*e_ast; 
-    int e_n_val;
-    st_to_hyb(ist, jst, ast, n_val, n_row, n_col, max, eData, offset, &e_ist, &e_jst, &e_ast, &e_n_val);
+    int n_max = 3;
+    int* ist; 
+    int* jst;
+    double* ast;
+    int n_val = ell_to_st((double**)eData, (int**)offset, n_row, n_max, &ist, &jst, &ast);
+    for(int i = 0; i < n_val; i++){
+        printf("%d\t%d\t%lf\n", ist[i], jst[i], ast[i]);
+    }
+
+    // int ist[9] = {0,0,1,1,2,2,2,3,3};
+    // int jst[9] = {0,1,1,2,0,2,3,1,3};
+    // double ast[9] = {1,7,2,8,5,3,9,6,4};
+    // int n_val = 9;
+    // int n_row = 4;
+    // int n_col = 4;
+    // int max = 2; 
+    // double** eData;
+    // double** offset; 
+    // int* e_ist;
+    // int* e_jst; 
+    // double*e_ast; 
+    // int e_n_val;
+    // st_to_hyb(ist, jst, ast, n_val, n_row, n_col, max, eData, offset, &e_ist, &e_jst, &e_ast, &e_n_val);
 
 }
