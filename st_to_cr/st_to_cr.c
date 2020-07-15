3501b6e89f06ed63ce74812bed6e5a56054fff4d
Gavin <gavinwzmails@gmail.com>
2020-07-15 14:45:12 +0800

    st to cr

--- st_to_cr/st_to_cr.c
+++ st_to_cr/st_to_cr.c
@@ -22,6 +22,26 @@ void st_read(char* filename, st_fmt* st, int* n_row, int* n_col){
     fclose(file);
 }
 
+void st_to_cr(st_fmt st, cr_fmt* cr, int n_row){
+    int index = 1;
+    int tag = st.ist[0];
+    (*cr).rcr = (int*)malloc(sizeof(int) * n_row + 1);
+    (*cr).ccr = (int*)malloc(sizeof(int) * st.n_val);
+    (*cr).vcr = (double*)malloc(sizeof(double) * st.n_val);
+    (*cr).rcr[0] = 0;
+    for(int i = 0; i < st.n_val; i++){
+        (*cr).ccr[i] = st.jst[i];
+        (*cr).vcr[i] = st.ast[i];
+        if(st.ist[i] != tag){
+            tag = st.ist[i];
+            (*cr).rcr[index] = i;
+            index++;
+        }
+    }
+    (*cr).rcr[index] = st.n_val;
+    (*cr).n_row = index;
+}
+
 void st_write(char* filename,st_fmt st){
     FILE* file = fopen(filename, "w+");
     int k;
@@ -59,24 +79,4 @@ void cr_write(char* filename,cr_fmt cr){
     return;
 }
 
-
-void st_to_cr(st_fmt st, cr_fmt* cr, int n_row){
-    int index = 1;
-    int tag = st.ist[0];
-    (*cr).rcr = (int*)malloc(sizeof(int) * n_row + 1);
-    (*cr).ccr = (int*)malloc(sizeof(int) * st.n_val);
-    (*cr).vcr = (double*)malloc(sizeof(double) * st.n_val);
-    (*cr).rcr[0] = 0;
-    for(int i = 0; i < st.n_val; i++){
-        (*cr).ccr[i] = st.jst[i];
-        (*cr).vcr[i] = st.ast[i];
-        if(st.ist[i] != tag){
-            tag = st.ist[i];
-            (*cr).rcr[index] = i;
-            index++;
-        }
-    }
-    (*cr).rcr[index] = st.n_val;
-    (*cr).n_row = index;
-}
 
