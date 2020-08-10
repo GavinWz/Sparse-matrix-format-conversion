38abf8ff11d6ece47cf927e2278323d4a471c361
Gavin <gavinwzmails@gmail.com>
2020-08-10 18:42:41 +0800

    add time record (st to cr)

--- st_to_cr/st_to_cr.c
+++ st_to_cr/st_to_cr.c
@@ -1,10 +1,11 @@
 #include"st_to_cr.h"
 
-void st_read(char* filename, st_fmt* st, int* n_row, int* n_col){
+clock_t st_read(char* filename, st_fmt* st, int* n_row, int* n_col){
+    clock_t begin = clock();
     FILE *file = fopen(filename, "r");
     if(file == NULL){
         printf("Can't open the input file.\n");
-        return;
+        return -1;
     }
     int n_val;
     fscanf(file, "%d%d%d", &(*n_row), &(*n_col), &n_val);
@@ -20,11 +21,13 @@ void st_read(char* filename, st_fmt* st, int* n_row, int* n_col){
     }
     (*st).n_val = n_val;
     fclose(file);
+    clock_t end = clock();
+    return end - begin;
 }
 
 void st_cr_sort(st_fmt* st, int left, int right){
     if(left >= right)
-        return;
+        return ;
     int i = left, j = right;
     int i_key = st->ist[i];
     int j_key = st->jst[i];
@@ -53,7 +56,8 @@ void st_cr_sort(st_fmt* st, int left, int right){
     st_cr_sort(st, i + 1, right);
 }
 
-void st_to_cr(st_fmt st, cr_fmt* cr, int n_row){
+clock_t st_to_cr(st_fmt st, cr_fmt* cr, int n_row){
+    clock_t begin = clock();
     int index = 1;
     int tag = st.ist[0];
     (*cr).rcr = (int*)malloc(sizeof(int) * n_row + 1);
@@ -71,30 +75,34 @@ void st_to_cr(st_fmt st, cr_fmt* cr, int n_row){
     }
     (*cr).rcr[index] = st.n_val;
     (*cr).n_row = index;
+    clock_t end = clock();
+    return end - begin;
 }
 
-void st_write(char* filename,st_fmt st){
+clock_t st_write(char* filename,st_fmt st){
+    clock_t begin = clock();
     FILE* file = fopen(filename, "w+");
     int k;
-    fprintf (file, "\nST: sparse triplet,    I, J,  A.\n" );
-    fprintf (file, "  The matrix in ST format:\n" );
-    fprintf (file, "     I     J       A\n" );
-    fprintf (file, "  ----  ----  --------------\n" );
-    fprintf (file, "\n" );
+    fprintf (file, "\nThe matrix in ST format:\n");
+    fprintf (file, "  Number of non-zero elements: %d\n", st.n_val );
+    fprintf (file, "\n   Rows  Cols   Values\n" );
+    fprintf (file, "   ----  ----  --------------\n" );
     for ( k = 0; k < st.n_val; k++ )
     {
         fprintf (file, "  %4d  %4d  %10.5lf\n", st.ist[k], st.jst[k], st.ast[k] );
     }
     fclose(file);
-    return;
+    clock_t end = clock();
+    return end - begin;
 }
 
-void cr_write(char* filename,cr_fmt cr){
+clock_t cr_write(char* filename,cr_fmt cr){
+    clock_t begin = clock();
     FILE* file = fopen(filename, "a");
     int k;
     fprintf (file, "\nCR: sparse triplet, Row, Col, Value.\n" );
     fprintf (file, "  The matrix in CR format:\n" );
-    fprintf (file, "   Row    Col    Value \n" );
+    fprintf (file, "   Rows   Cols   Values \n" );
     fprintf (file, "   ----   ----   ----  \n" );
     for ( k = 0; k < cr.rcr[cr.n_row]; k++ )
     {
@@ -107,24 +115,43 @@ void cr_write(char* filename,cr_fmt cr){
 
     
     fclose(file);
-    return;
+    clock_t end = clock();
+    return end - begin;
+}
+
+void time_write(char* filename, clock_t read_t, clock_t convert_t, clock_t write_t1, clock_t write_t2){
+    FILE* file = fopen(filename, "a");
+    fprintf(file, "\n------------------------------------\n");
+    fprintf(file, "-------------Time Sheet-------------\n");
+    if(read_t == -1){
+        fprintf(file, "Input file opening failed\n");
+        fprintf(file, "\n------------------------------------\n");
+        return;
+    }
+    fprintf(file, "Data Read:\t\t%ld ms\n", read_t);
+    fprintf(file, "Format Conversion:\t%ld ms\n", convert_t);
+    fprintf(file, "CC Write:\t\t%ld ms\n", write_t1);
+    fprintf(file, "ST Write:\t\t%ld ms\n", write_t2);
+    fprintf(file, "------------------------------------\n");
 }
 
 void st_to_cr_run(char* ifilename, char* ofilename){
     st_fmt st;
+    cr_fmt cr;
     int n_row;
     int n_col;
-    int max;
-    st_read(ifilename, &st, &n_row, &n_col);
-    // st_write (ofilename, st);
-    // printf("\n\n");
+    clock_t st_read_t, cr_write_t, sort_t, st_write_t, convert_t;
 
+    st_read_t = st_read(ifilename, &st, &n_row, &n_col);
+
+    clock_t begin = clock();
     st_cr_sort(&st, 0, st.n_val-1);
-
-    // st_cr_sort(&st);
-    cr_fmt cr;
-
-    st_to_cr(st, &cr, n_row);
-    st_write (ofilename, st);
-    cr_write(ofilename, cr);
+    clock_t end = clock();
+    sort_t = end - begin;
+    
+    convert_t = st_to_cr(st, &cr, n_row);
+    st_write_t = st_write (ofilename, st);
+    cr_write_t = cr_write(ofilename, cr);
+    
+    time_write(ofilename, st_read_t, convert_t + sort_t, st_write_t, cr_write_t);
 }
--- st_to_cr/st_to_cr.h
+++ st_to_cr/st_to_cr.h
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<time.h>
 
 typedef struct st{
     int *ist;
@@ -15,12 +16,14 @@ typedef struct cr{
     int n_row;
 } cr_fmt;
 
-void st_read(char* filename, st_fmt* st, int* n_row, int* n_col);
+clock_t st_read(char* filename, st_fmt* st, int* n_row, int* n_col);
 
-void st_write(char* filename,st_fmt st);
+clock_t st_write(char* filename,st_fmt st);
 
-void cr_write(char* filename,cr_fmt cr);
+void st_cr_sort(st_fmt* st, int left, int right);
 
-void st_to_cr(st_fmt st, cr_fmt* cr, int n_row);
+clock_t cr_write(char* filename,cr_fmt cr);
+
+clock_t st_to_cr(st_fmt st, cr_fmt* cr, int n_row);
 
 void st_to_cr_run(char* ifilename, char* ofilename);
