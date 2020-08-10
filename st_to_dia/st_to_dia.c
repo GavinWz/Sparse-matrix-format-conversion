cce8b441e4f7c97bc34b6f56251361502591777f
Gavin <gavinwzmails@gmail.com>
2020-08-10 13:41:31 +0800

    update dia

--- st_to_dia/st_to_dia.c
+++ st_to_dia/st_to_dia.c
@@ -5,24 +5,24 @@
 # include <string.h>
 #include "st_to_dia.h"
 
-void dpre_usconv_st2dia(int m,int n,double* ast,int n_ast,int* ist,int* jst,int* LDA,int* NDIAG, int **idiag, double **val_dia)
+void dpre_usconv_st2dia(int m, int n, st_fmt st, dia_fmt* dia)
 {
-  *NDIAG=0;
-  *LDA=m<n?m:n;
+  dia->ndiag=0;
+  dia->ldiag=m<n?m:n;
   int num_rows,num_cols,num_nonzeros;
   int i,ii,jj,offset,map_index,complete_ndiags,VAL_DIA_size;
   const int unmarked = -1;
   num_rows = m;
   num_cols = n;
-  num_nonzeros = n_ast;
+  num_nonzeros = st.n_val;
   complete_ndiags = 0;
   int* diag_map = (int*)malloc(sizeof(int)*(num_rows+num_cols));     //mark the diags
   int* diag_offset = (int*)malloc(sizeof(int)*(num_cols+num_rows));
   memset(diag_map,unmarked,sizeof(int)*(num_rows+num_cols));
   memset(diag_offset,unmarked,sizeof(int)*(num_rows+num_cols));
   for(i=0;i<num_nonzeros;i++){
-    ii = ist[i];
-    jj = jst[i];
+    ii = st.ist[i];
+    jj = st.jst[i];
     map_index = num_rows-ii+jj;            //used to find the same diag
     if(diag_map[map_index] == unmarked){        
       diag_map[map_index] = complete_ndiags;
@@ -30,8 +30,8 @@ void dpre_usconv_st2dia(int m,int n,double* ast,int n_ast,int* ist,int* jst,int*
       complete_ndiags++;                 //number of diags
     }
   }
-  *NDIAG = complete_ndiags;
-  VAL_DIA_size=(*NDIAG)*(*LDA);
+  dia->ndiag = complete_ndiags;
+  VAL_DIA_size=dia->ndiag*dia->ldiag;
   int *IDIAG=(int*)malloc(sizeof(int)*(complete_ndiags));
   double *VAL_DIA=(double*)malloc(sizeof(double)*VAL_DIA_size);
   memset(IDIAG,0,sizeof(int)*(complete_ndiags));
@@ -42,78 +42,74 @@ void dpre_usconv_st2dia(int m,int n,double* ast,int n_ast,int* ist,int* jst,int*
     }
   }
   for(i=0;i<num_nonzeros;i++){    //get values of every diag
-    ii = ist[i];
-    jj = jst[i];
+    ii = st.ist[i];
+    jj = st.jst[i];
     map_index = num_rows-ii+jj;
     int diag = diag_map[map_index];
-    if((*LDA)-(num_rows-ii)>0){
-      offset=(*LDA)-(num_rows-ii);
+    if(dia->ldiag-(num_rows-ii)>0){
+      offset=dia->ldiag-(num_rows-ii);
     }else{
       offset=0;
     }
-    VAL_DIA[diag*(*LDA)+offset] = ast[i];
+    VAL_DIA[diag*dia->ldiag+offset] = st.ast[i];
   }
  
   free(diag_map);
   free(diag_offset);
-  *val_dia=VAL_DIA;  //Take the result out of the function
-  *idiag=IDIAG;
+  dia->val_dia=VAL_DIA;  //Take the result out of the function
+  dia->idiag=IDIAG;
 }
 
-
-
-
-int st_read(char* filename,double **ast,int **ist, int **jst, int *n_row, int *n_col, int *n_val){
-    
+void st_read(char* filename, st_fmt* st, int* n_row, int* n_col){
     FILE *file = fopen(filename, "r");
     if(file == NULL){
-        printf("Can't open the file.\n");
-        return -1;
+        printf("Can't open the input file.\n");
+        return;
     }
-    fscanf(file, "%d%d%d", &(*n_row), &(*n_col), &(*n_val));
-    *ast = (double*)malloc(sizeof(double) * (*n_val));
-    *ist = (int*)malloc(sizeof(int) * (*n_val));
-    *jst = (int*)malloc(sizeof(int) * (*n_val));
+    int n_val;
+    fscanf(file, "%d%d%d", &(*n_row), &(*n_col), &n_val);
+    
+    (*st).ast = (double*)malloc(sizeof(double) * n_val);
+    (*st).ist = (int*)malloc(sizeof(int) * n_val);
+    (*st).jst = (int*)malloc(sizeof(int) * n_val);
+    
     int index = 0;
     while(feof(file) == 0){
-        fscanf(file, "%d%d%lf", &(*ist)[index], &(*jst)[index], &(*ast)[index]);
+        fscanf(file, "%d%d%lf", &((*st).ist[index]), &((*st).jst[index]), &((*st).ast[index]));
         index++;
     }
+    (*st).n_val = n_val;
     fclose(file);
-    return index;
 }
 
-void st_write(char* filename,  int m, int n, int nst, int ist[], int jst[], double ast[], 
-  char *title ){
+void st_write(char* filename,st_fmt st){
     FILE* file = fopen(filename, "w+");
-    fprintf (file, "Convert a sparse matrix from ST to DIA format.\n" );
-    fprintf (file, "ST: sparse triplet,    I, J,  A.\n" );
     int k;
-    fprintf (file, "\n" );
-    fprintf (file, "%s\n", title );
+    fprintf (file, "\nST: sparse triplet,    I, J,  A.\n" );
+    fprintf (file, "  The matrix in ST format:\n" );
     fprintf (file, "     #     I     J       A\n" );
     fprintf (file, "  ----  ----  ----  --------------\n" );
     fprintf (file, "\n" );
-    for ( k = 0; k < nst; k++ )
+    for ( k = 0; k < st.n_val; k++ )
     {
-        fprintf (file, "  %4d  %4d  %4d  %10.8g\n", k, ist[k], jst[k], ast[k] );
+        fprintf (file, "  %4d  %4d  %10.5lf\n", st.ist[k], st.jst[k], st.ast[k] );
     }
+    fprintf (file, "\n" );
     fclose(file);
     return;
 }
 
-void dia_write(char* filename, double *val, int *indx, int lda, int ndiag){
+void dia_write(char* filename, dia_fmt dia){
     FILE* file = fopen(filename, "a");
-    if(file == NULL){
-        printf("Can't open the file: %s.\n", filename);
+    if(!file){
+        fprintf(file, "Can not open the output file.");
         return;
     }
-    int i, j;
-    fprintf(file, "\nDIA Matrix:\n");
-    for(i = 0; i < ndiag; i++){
-        fprintf(file, "%3d:    ", indx[i]);
-        for(j = i * lda; j < (i+1) * lda; j++){
-            fprintf(file, "%lf    ", val[j]);
+    fprintf(file, "\nThe DIA martrix is: \n");
+    for(int i = 0; i < dia.ndiag; i++){
+        fprintf(file, "%4d: ", dia.idiag[i]);
+        for(int j = 0; j < dia.ldiag; j++){
+            fprintf(file, "%lf ", dia.val_dia[i*dia.ldiag+j]);
         }
         fprintf(file, "\n");
     }
@@ -124,11 +120,11 @@ void st_to_dia_run(char* readFile, char* writeFile){
     double *ast;
     int *ist, *jst;
     int n_row, n_col, n_val;
-    st_read(readFile, &ast, &ist, &jst, &n_row,  &n_col, &n_val);
-    int lda, ndiag;
-    int* idiag;
-    double* val_dia;
-    st_write (writeFile, n_row, n_col, n_val, ist, jst, ast, "  The matrix in ST format:" );
-    dpre_usconv_st2dia(n_row, n_col, ast, n_val, ist, jst, &lda, &ndiag, &idiag, &val_dia);
-    dia_write(writeFile, val_dia, idiag, lda, ndiag);
+
+    st_fmt st;
+    dia_fmt dia;
+    st_read(readFile, &st, &n_row, &n_col);
+    dpre_usconv_st2dia(n_row, n_col, st, &dia);
+    st_write(writeFile,st);
+    dia_write(writeFile, dia);
 }
--- st_to_dia/st_to_dia.h
+++ st_to_dia/st_to_dia.h
@@ -1,10 +1,24 @@
-void dpre_usconv_st2dia  (int m,int n,double* VAL,int n_VAL,int* INDX,int* JNDX,
-    int* LDA,int* NDIAG, int **idiag, double **val_dia);
-void st_print ( int m, int n, int nst, int ist[], int jst[], double ast[], 
-    char *title );
-void dia_print (double *val, int n_val, int *indx, int lda, int ndiag);
+typedef struct dia{
+    int ldiag;  //对角线长度
+    int ndiag;  //对角线个数
+    int* idiag; //对角线编号数组
+    double* val_dia; //DIA所有元素值组成的一维数组
+    int n_val;  //非零元个数
+} dia_fmt;
+
+typedef struct st{
+    int *ist;
+    int *jst;
+    double *ast;
+    int n_val;
+} st_fmt;
+
+void dpre_usconv_st2dia(int m, int n, st_fmt st, dia_fmt* dia);
+
+void st_read(char* filename, st_fmt* st, int* n_row, int* n_col);
+
+void st_write(char* filename,st_fmt st);
+
+void dia_write(char* filename, dia_fmt dia);
+
 void st_to_dia_run(char* readFile, char* writeFile);
-void dia_write(char* filename, double *val, int *indx, int lda, int ndiag);
-void st_write(char* filename,  int m, int n, int nst, int ist[], int jst[], double ast[], char *title);
-void test01 ( );
-void test02 ( );
